54cc0bd997dc725b4147de99812747eb33450b95
Alhai <aly.haidara@seira-formation.fr>
2024-06-11 13:30:00 +0200

    map

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,67 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import MapView, { Polyline } from "react-native-maps";
+import { ScrollView, StatusBar, StyleSheet, View } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const coordsA = [
+    { latitude: 45.184269, longitude: 5.756467 }, // A
+    { latitude: 45.184658, longitude: 5.756699 }, // A
+    { latitude: 45.184906, longitude: 5.75683 }, // A
+    { latitude: 45.184904, longitude: 5.757454 }, // A
+    { latitude: 45.184616, longitude: 5.757479 }, // A
+    { latitude: 45.184287, longitude: 5.757321 }, // A
+    { latitude: 45.184524, longitude: 5.757299 }, // A
+    { latitude: 45.184648, longitude: 5.756841 }, // A
+  ];
+
+  const coordsH = [
+    { latitude: 45.184745, longitude: 5.760324 }, // H
+    { latitude: 45.183496, longitude: 5.75922 }, // H
+    { latitude: 45.183804, longitude: 5.759389 }, // H
+    { latitude: 45.183588, longitude: 5.760236 }, // H
+    { latitude: 45.184422, longitude: 5.760982 }, // H
+    { latitude: 45.183274, longitude: 5.760067 }, // H
+  ];
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: 45.184269,
+              longitude: 5.756467,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Polyline
+              coordinates={coordsA}
+              strokeColor="#FF0000"
+              strokeWidth={6}
+            />
+            <Polyline
+              coordinates={coordsH}
+              strokeColor="#0000FF"
+              strokeWidth={6}
+            />
+          </MapView>
+        </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  map: {
+    width: "100%",
+    height: "100%",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  scrollView: {
+    marginHorizontal: 20,
   },
 });
